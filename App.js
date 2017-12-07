8e205dc23c9ce510ceda07d06225398215d8b858
Wellyngton Amaral <wellington.amaral.ufc@gmail.com>
2017-12-07 16:06:52 -0200

    update version

--- App.js
+++ App.js
@@ -1,57 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import {Navigation} from 'react-native-navigation';
+import {registerScreens} from './src/screens';
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+registerScreens();
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+export const App = Navigation.startTabBasedApp({
+  tabs: [
+    {
+      label: 'Signin',
+      screen: 'page.Signin',
+      icon: require('./src/images/icons/unlocked.png'),
+      title: 'Signin'
+    },
+    {
+      label: 'Home',
+      screen: 'page.Home',
+      icon: require('./src/images/icons/home.png'),
+      title: 'Home'
+    }
+  ]
 });
